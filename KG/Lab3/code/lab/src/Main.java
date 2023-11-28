7558c20d559071639379c2c56cddb6d2e0b8ea4d
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2023-11-28 15:37:16 +0300

    KG 3

--- KG/Lab3/code/lab/src/Main.java
+++ KG/Lab3/code/lab/src/Main.java
@@ -5,15 +5,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Pixel> points = Methods.brezenheim(5,5,13,9);
-        for (int i = 0; i < points.size(); i++) {
-            System.out.println(points.get(i).toString());
-        }
-        System.out.println("----------------------");
-        ArrayList<Pixel> points1 = Methods.stepByStep(5,5,13,9 );
-        for (int i = 0; i < points1.size(); i++) {
-            System.out.println(points1.get(i).toString());
-        }
         MainFrame mainFrame = new MainFrame();
         mainFrame.setSize(400,100);
         mainFrame.setVisible(true);
